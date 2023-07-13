022e0f6ff54bfcea35e8766efef84c82eceff0ae
hugo <hugo@hugos-Air.homenet.telecomitalia.it>
2023-07-13 15:48:44 +0200

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,12 +3,14 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { appWithTranslation } from 'next-i18next';
 import "@fontsource/inter" // Importe a fonte Inter
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
       <ToastContainer position="top-right" />
+      <Analytics />
     </>
   );
 }
